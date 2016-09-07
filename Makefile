4af9c61b5a1da7a1816bdb4c51ff1c7f9ade7200
Shanbo Cheng <cshanbo@gmail.com>
2016-09-07 13:15:41 +0800

    first commit

--- Makefile
+++ Makefile
@@ -2,16 +2,24 @@
 #
 CXX=g++
 # CXXFLAGS = -g -std=c++11 -stdlib=libstdc++ -lpcrecpp -Wall -pedantic -I.
-CXXFLAGS = -g -std=c++11 -lpcrecpp -Wall -pedantic  -I.
+CXXFLAGS = -g -std=c++11 -Wall -pedantic -I.
 INC_DIR = include
 SRC_DIR = src
 OBJ_DIR = obj
+BIN_DIR = bin
 
-test: $(SRC_DIR)/main.cpp 
-	$(CXX) $(CXXFLAGS) $(SRC_DIR)/main.cpp -o test 
+$(BIN_DIR)/test: | $(BIN_DIR) $(OBJ_DIR) $(SRC_DIR)/main.cpp $(OBJ_DIR)/util.o
+	$(CXX) $(CXXFLAGS) $(SRC_DIR)/main.cpp -lpcrecpp -o $(BIN_DIR)/test $(OBJ_DIR)/util.o
 
-# $(OBJ_DIR)/main.o: $(SRC_DIR)/main.cpp
-# 	$(CXX) $(CXXFLAGS) -c $(SRC_DIR)/main.cpp -o $(OBJ_DIR)/main.o -lpcrecpp
+$(BIN_DIR):
+	mkdir -p $(BIN_DIR)
+
+$(OBJ_DIR):
+	mkdir -p $(OBJ_DIR)
+
+$(OBJ_DIR)/util.o: $(SRC_DIR)/util.cpp $(INC_DIR)/util.h
+	$(CXX) $(CXXFLAGS) -c $(SRC_DIR)/util.cpp -o $(OBJ_DIR)/util.o
+
 clean:
 	rm -rf $(OBJ_DIR)/*.o
-	rm -rf test
+	rm -rf $(BIN_DIR)/*
--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,46 @@
 Program: test
 Description: 
 Date: 2016-07-05 21:07:39
-Last modified: 2016-09-06 22:07:46
+Last modified: 2016-09-07 11:09:04
 GCC version: 4.7.3
 */
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <pcrecpp.h>
 
+#include "../include/util.h"
+
 using namespace std;
 
-int main() {
-   int i;
-   string s;
-   pcrecpp::RE re("(\\w+):(\\d+)");
-   if (re.error().length() > 0) {
-	  cout << "PCRE compilation failed with error: " << re.error() << "\n";
-   }
-   if (re.PartialMatch("root:1234", &s, &i))
-   cout << s << " : " << i << "\n";
+int show_usage() {
+    std::cout << "usage: cmd + input_file + result_file" << std::endl;
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 3) {
+        show_usage();
+        exit(0);
+    }
+
+    string root_dir = "../conf";
+    std::ifstream ifs(argv[1], std::ios::in);
+
+    string line = "";
+    while(ifs) {
+        if(getline(ifs, line)) {
+            trim(line);
+            cout << line << endl;
+        }
+    }
+    int i;
+    string s;
+    pcrecpp::RE re("(\\w+):(\\d+)");
+    if (re.error().length() > 0) {
+        std::cout << "PCRE compilation failed with error: " << re.error() << "\n";
+    }
+    if (re.PartialMatch("root:1234", &s, &i))
+    std::cout << s << " : " << i << std::endl;
 }
